e9ce17a91542e73cd20209f5e2da116b9f3a207d
BT_1N3_16 <fwilking4736@bths.edu>
2019-01-30 08:20:58 -0500

    created class files

---
+++ src/merge.java
@@ -0,0 +1,14 @@
+public class merge {
+
+    private int[] arr1;
+    private int[] arr2;
+
+    public merge(int[] arr1, int[] arr2){
+        this.arr1 = arr1;
+        this.arr2 = arr2;
+    }
+
+    public int[] sort(){
+        
+    }
+}
