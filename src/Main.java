2f49ab4c56fe9bb88bde933beab1794b332f4c50
zfeng7527 <zfeng7527@bths.edu>
2019-02-04 08:32:03 -0500

    JJJ

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,15 @@
 public class Main{
     public static void main(String[] args){
-        int[] arr1 = Utilities.sortedArr(100);
-        int[] arr2 = Utilities.sortedArr(100);
+        int[] arr1 = Utilities.PartSortedArray(100);
+        int[] arr2 = Utilities.PartSortedArray(10000);
 
         Utilities.printIntArr(arr1);
         Utilities.printIntArr(arr2);
+
+        if(Utilities.isSorted(arr2)){
+            System.out.println("Sorted");
+        }
+        else System.out.println("Not Sorted");
 
         merge merger = new merge(arr1, arr2);
         int[] merged = merger.sort();
